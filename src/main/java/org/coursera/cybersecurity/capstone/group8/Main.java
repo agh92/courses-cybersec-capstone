501583c5031f5bb68c335bd590d70729b64636a6
alx <alx27alx@gmail.com>
2017-09-23 12:54:06 +0800

    Config fixed

--- src/main/java/org/coursera/cybersecurity/capstone/group8/Main.java
+++ src/main/java/org/coursera/cybersecurity/capstone/group8/Main.java
@@ -1,7 +1,13 @@
 package org.coursera.cybersecurity.capstone.group8;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class Main {
 
 	public static void main(String[] args) {
